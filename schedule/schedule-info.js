5c338f7a3d3d51543da8b79061d36d178576d6ce
Mark Fontenot <mfonten@lyle.smu.edu>
2015-12-17 07:10:19 -0600

    Initial commit

---
+++ schedule/schedule-info.js
@@ -0,0 +1,4 @@
+moreaEventData = [
+  {title: "Experience the Baz", url: "/16S-CSE2341/morea/baz/experience.html", start: "2015-06-11T18:00"},
+  {title: "Learn to Baz", url: "/16S-CSE2341/modules/baz", start: "2015-06-11", end: "2015-06-15"}
+]
